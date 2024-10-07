31ede870984ad48350b1a39e9bec80122e5465c8
sbx_user1051 <sbx_user1051@169.254.64.37>
2024-10-07 15:00:58 +0000

    Mon Oct 07 15:00:58 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,8 +1,24 @@
 from solutions.CHK import checkout_solution
+import pytest
 
+
+@pytest.fixture
+def prices():
+    return {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
 
 class TestCheckout():
-    def test_single_item(self):
-        assert checkout_solution.checkout("1A") == 50
+    def test_single_item(self, prices):
+        assert checkout_solution.checkout("1A") == prices["A"]
+
+    def test_all_items(self, prices):
+        checkout_string = ",".join(map(lambda x: f"1{x}", prices.keys()))
+        checkout_price = sum(prices.values())
+        assert checkout_solution.checkout(checkout_string) == checkout_price
+
 
 
